0cbc9ef1da0cdf56e9718bdd55ae4f4cfdb0f352
class <class@192.168.1.44>
2022-06-15 09:46:51 +0900

    テスト西浦

--- src/main/java/servlet/TopServlet.java
+++ src/main/java/servlet/TopServlet.java
@@ -38,19 +38,19 @@ public class TopServlet extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		
+
 		//セッションスコープにオブジェクトの保存
 		HttpSession session = request.getSession();
-		
+
 		//問題リストと回答リストの要素取得のためのインデックスとなる数
 		Integer cnt = 0;
-		
+
 		//リクエストパラメータを取得
 		request.setCharacterEncoding("UTF-8");
 		String name = request.getParameter("name");
 		String pref = request.getParameter("pref");
 		String city = request.getParameter("city");
-		
+
 		String errMsg = "";
 		if (name == null || name.length() == 0) {
 			errMsg += "名前が入力されていません<br>";
@@ -69,6 +69,8 @@ public class TopServlet extends HttpServlet {
 			dispatcher.forward(request, response);
 		}
 
+		//追加。西浦
+
 		//個人情報をプロパティに設定
 		UserModel userModel = new UserModel(name, pref, city);
 
@@ -84,10 +86,10 @@ public class TopServlet extends HttpServlet {
 		qList.add(q3);
 		qList.add(q4);
 		qList.add(q5);
-		
+
 		//回答を持つリストの作成
 		List<String> ansList = new ArrayList<>();
-		
+
 		//セッションスコープにオブジェクトの保存
 		session.setAttribute("qList", qList);
 		session.setAttribute("ansList", ansList);
